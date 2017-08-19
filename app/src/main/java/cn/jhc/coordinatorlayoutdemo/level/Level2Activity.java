53acc57903db7b3786fc549f7952d9b693a84e2b
NataliePortman <1401010800@qq.com>
2017-08-19 15:39:05 +0800

    完成了在CoordinatorLayout根部局下AppBarLayout与Recycler的响应

--- app/src/main/java/cn/jhc/coordinatorlayoutdemo/level/Level2Activity.java
+++ app/src/main/java/cn/jhc/coordinatorlayoutdemo/level/Level2Activity.java
@@ -1,9 +1,11 @@
 package cn.jhc.coordinatorlayoutdemo.level;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,19 +19,29 @@ public class Level2Activity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerAdapter adapter;
     private List<String> data;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level2);
-        data = new ArrayList<>();
-        for (int i = 1; i <= 50; i++) {
-            data.add("this is " + i);
-        }
+        initData();
+
         recyclerView = (RecyclerView) findViewById(R.id.level2_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new RecyclerAdapter(data);
         recyclerView.setAdapter(adapter);
         recyclerView.setNestedScrollingEnabled(true);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+        toolbar.setTitle("AppBarLayout与CoordinatorLayout结合");
+        setSupportActionBar(toolbar);
+    }
+
+    private void initData() {
+        data = new ArrayList<>();
+        for (int i = 1; i <= 50; i++) {
+            data.add("this is " + i);
+        }
     }
 }
